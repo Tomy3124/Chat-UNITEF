26a4bbf6c9d6c2dbce447a559aa525666cb2fe16
Luis Edwardo Ruiz <Luis Edwardo Ruiz@DESKTOP-7GRNF03>
2026-03-20 14:44:26 -0400

    Fix ASGI initialization order

--- intranet/asgi.py
+++ intranet/asgi.py
@@ -4,12 +4,12 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
-from intranet.routing import websocket_urlpatterns
-
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "intranet.settings")
 
 django_asgi_app = get_asgi_application()
+
+from intranet.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter(
     {
